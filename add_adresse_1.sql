0e1c40456d4f5f36388ed56dfdae847f8d76cc76
Artjom Styron <astyron@novomind.com>
2025-04-28 12:43:56 +0200

    feat(kundeadresse): add entity, remove in kunde adresse info

---
+++ add_adresse_1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE kundeadresse (
+    adresseID SERIAL NOT NULL,
+    kundennnr INTEGER NOT NULL,
+    stadt TEXT NOT NULL,
+    strasse TEXT NOT NULL,
+    hausnr INTEGER NOT NULL,
+    plz INTEGER NOT NULL,
+    FOREIGN KEY (kundennnr) REFERENCES kunde(kundennr) ON DELETE CASCADE
+);
+
+ALTER TABLE kunde
+DROP COLUMN strasse,
+DROP COLUMN hausnr,
+DROP COLUMN  plz,
+DROP COLUMN ort;
