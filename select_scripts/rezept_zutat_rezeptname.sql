714ac4d6d36815ce9d8e3c4889c7a28acfc8af03
Artjom Styron <astyron@novomind.com>
2025-04-29 10:33:38 +0200

    feat(select): rezept zutat per rezeptname

--- select_scripts/rezept_zutat_rezeptname.sql
+++ select_scripts/rezept_zutat_rezeptname.sql
@@ -1,4 +1,4 @@
-SELECT bezeichnung, menge FROM zutat
+SELECT bezeichnung as Lachs_mit_Reis, menge FROM zutat
 INNER JOIN rezept_zutat ON zutat.zutatennr = rezept_zutat.zutatennr
 INNER JOIN rezept ON rezept_zutat.rezeptid = rezept.rezeptid
 WHERE rezeptname = 'Lachs mit Reis';
